2da3b987cf8e3d00028d7fefff121ea875b5c158
Emeateuve <emeateuve@gmail.com>
2018-04-25 09:31:07 +0200

    creando estructura

--- ProyectoAngular/src/app/app.module.ts
+++ ProyectoAngular/src/app/app.module.ts
@@ -1,24 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, RouterLink, Routes } from "@angular/router";
 
+import { PokeserviceService } from "./pokeservice.service";
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { PokedexesComponent } from './pokedexes/pokedexes.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { MovesComponent } from './moves/moves.component';
 
+const appRoutes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'pokedexes', component: PokedexesComponent},
+  {path: 'moves', component: MovesComponent},
+  {path: 'pokemon/:pokeID', component: PokemonComponent},
+  {path: 'moves/:moveID', component: MovesComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    PokedexesComponent,
+    PokemonComponent,
+    MovesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: false})
   ],
-  providers: [],
+  providers: [PokeserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ProyectoAngular/src/app/pokedexes/pokedexes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedexes',
+  templateUrl: './pokedexes.component.html',
+  styleUrls: ['./pokedexes.component.css']
+})
+export class PokedexesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
