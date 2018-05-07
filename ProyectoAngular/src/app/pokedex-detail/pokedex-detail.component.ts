ca0675861db77216abc5cd2ae89d007e78b27b60
Emeateuve <emeateuve@gmail.com>
2018-05-08 01:15:03 +0200

    Maquetando detalle de pokemons desde pokedex

--- ProyectoAngular/src/app/pokedex-detail/pokedex-detail.component.ts
+++ ProyectoAngular/src/app/pokedex-detail/pokedex-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PokeserviceService} from "../pokeservice.service";
 import {PokedexesComponent} from "../pokedexes/pokedexes.component";
 import {ActivatedRoute} from "@angular/router";
@@ -11,38 +11,45 @@ import {ActivatedRoute} from "@angular/router";
 })
 
 
-
 export class PokedexDetailComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private pokePetition: PokeserviceService, public pokedexComponent: PokedexesComponent) {
 
   }
+
   public pokedexName;
   public pokedex;
   public pokemonsArray;
+  public pokeReturn;
+  public pokeBoolean;
 
-  pokemonid = 'hola'
 
   ngOnInit() {
     this.route.params.subscribe(parameter => {
       this.pokedexName = parameter['pokedexID'];
       this.loadPokedex();
     });
+    this.pokeBoolean = true;
   }
 
-  loadPokedex(){
-    this.pokePetition.pokedexesSearch(this.pokedexName).subscribe(pokedexName =>{
+  loadPokedex() {
+    this.pokePetition.pokedexesSearch(this.pokedexName).subscribe(pokedexName => {
       this.pokedex = pokedexName;
       this.pokemonsArray = pokedexName.pokemon_entries;
-      console.log('Objeto devuelto: ', this.pokedex);
-      for (let i = 0; i < this.pokemonsArray.length; i++){
-        // console.log('pokemon: ',this.pokemonsArray[i].pokemon_species.name);
-      }
     })
   }
 
-  muestraid(event){
-    console.log('La id es:', event)
+  pokeCall(pokemon) {
+    if (this.pokeBoolean){
+      this.pokeBoolean = false;
+      this.pokePetition.pokeSearch(pokemon).subscribe(pokeData => {
+        this.pokeReturn = pokeData;
+        console.log('Desde el componente de vuelta', pokeData);
+        this.pokeBoolean = true;
+      })
+    }
+
   }
+
 
 }
