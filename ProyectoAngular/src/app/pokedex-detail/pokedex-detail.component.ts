31ca0a76f0edbd3d7d8a1230598e8054db066976
Emeateuve <emeateuve@gmail.com>
2018-05-09 01:07:52 +0200

    maquetando pokemon header mejorado

--- ProyectoAngular/src/app/pokedex-detail/pokedex-detail.component.ts
+++ ProyectoAngular/src/app/pokedex-detail/pokedex-detail.component.ts
@@ -4,8 +4,8 @@ import {PokedexesComponent} from "../pokedexes/pokedexes.component";
 import {ActivatedRoute} from "@angular/router";
 
 /* PokéDeclaring jQuery PokéVariables */
-declare var jquery:any;
-declare var $ :any;
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-pokedex-detail',
@@ -28,32 +28,32 @@ export class PokedexDetailComponent implements OnInit {
 
 
   ngOnInit() {
+    this.pokeBoolean = true;
     this.route.params.subscribe(parameter => {
       this.pokedexName = parameter['pokedexID'];
       this.loadPokedex();
     });
-    this.pokeBoolean = true;
   }
 
   loadPokedex() {
     this.pokePetition.pokedexesSearch(this.pokedexName).subscribe(pokedexName => {
       this.pokedex = pokedexName;
       this.pokemonsArray = pokedexName.pokemon_entries;
-    })
-  }
+    });
+  };
 
   pokeCall(pokemon) {
-    if (this.pokeBoolean){
+    if (this.pokeBoolean) {
       this.pokeBoolean = false;
       this.pokePetition.pokeSearch(pokemon).subscribe(pokeData => {
         this.pokeReturn = pokeData;
         this.pokeBoolean = true;
-      })
+      });
     }
-  }
+  };
 
-  pokeCollapse(){
+  pokeCollapse() {
     $('.collapse').collapse('hide')
-  }
+  };
 
 }
